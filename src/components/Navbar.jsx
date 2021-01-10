3f984b72a64dd97b67c90dd47199962fbb505f48
Danny <agyapongdaniel7777@gmail.com>
2021-01-10 18:05:31 +0000

    init commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            {/* amazon-logo */}
+            
+            {/* search-input */}
+            {/* search-icon-btn */}
+            {/* sign */}
+            {/* orders */}
+            {/* prime */}
+
+            
+        </div>
+    )
+}
+
+export default Navbar
