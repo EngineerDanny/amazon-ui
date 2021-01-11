d7186a1a3e2e1ca214d7c6c3f3bcc984de1a1b06
Danny <agyapongdaniel7777@gmail.com>
2021-01-11 22:38:59 +0000

    navbar completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Navbar.css";
-import SearchIcon from "@material-ui/icons/Search";
+import { Search, ShoppingBasket } from "@material-ui/icons";
 
 const Navbar = () => {
   return (
@@ -11,14 +11,34 @@ const Navbar = () => {
         src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
         alt="Amazon Logo"
       />
-
-      <input type="text" />
-      <SearchIcon className="search-icon" />
       {/* search-input */}
+      <input type="text" className="search-input" />
+
       {/* search-icon-btn */}
+      <Search className="search-icon" />
+
       {/* sign */}
+      <div className="wlc-section">
+        <span className="line-1">Hello,</span>
+        <span className="line-2">Sign In</span>
+      </div>
+
       {/* orders */}
+      <div className="wlc-section">
+        <span className="line-1">Returns</span>
+        <span className="line-2">& Orders</span>
+      </div>
+
       {/* prime */}
+      <div className="wlc-section">
+        <span className="line-1">Your</span>
+        <span className="line-2">Prime</span>
+      </div>
+
+      <div className="cart-section">
+        <ShoppingBasket />
+        <span className="cart-count">0</span>
+      </div>
     </div>
   );
 };
