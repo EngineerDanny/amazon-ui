70c3aa1891293f169680a5de1a297d3fda5e0f37
Danny <agyapongdaniel7777@gmail.com>
2021-01-11 16:33:34 +0000

    fix bug of not importing css file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import "./Navbar.css"
 
 const Navbar = () => {
   return (
     <div className="navbar">
       {/* amazon-logo */}
-      <img src="../" alt=""/>
-
+      {/* <img
+        className="logo"
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        alt="Amazon Logo"
+      /> */}
+      <h1>Hellow</h1>
       {/* search-input */}
       {/* search-icon-btn */}
       {/* sign */}
