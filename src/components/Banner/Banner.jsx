f6177c987488d508a42c86ad1b994c2b1ecb7adc
Danny <agyapongdaniel7777@gmail.com>
2021-01-11 23:36:06 +0000

    banner styling done

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import "./Banner.css";
 
 const Banner = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <div className="banner-container">
+      <img
+        className="banner-img"
+        src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2021/NYNY/Fuji_TallHero_NYNY_en_US_1x._CB412256579_.jpg"
+        alt="Banner"
+      />
+    </div>
+  );
+};
 
-export default Banner
+export default Banner;
