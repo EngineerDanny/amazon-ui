68d224091dc538fe2eb0b43f091881c5016dbf32
Danny <agyapongdaniel7777@gmail.com>
2021-01-13 16:25:20 +0000

    z-index error fix

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,12 @@ function App() {
       <Navbar />
       {/* Banner */}
       <div className="banner-container">
-      <img
-        className="banner-img"
-        src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2021/NYNY/Fuji_TallHero_NYNY_en_US_1x._CB412256579_.jpg"
-        alt="Banner"
-      />
-    </div>
+        <img
+          className="banner-img"
+          src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2021/NYNY/Fuji_TallHero_NYNY_en_US_1x._CB412256579_.jpg"
+          alt="Banner"
+        />
+      </div>
       {/* products */}
       <div className="gen-product-container">
         <Product />
