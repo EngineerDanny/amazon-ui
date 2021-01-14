68b9afef17b8a61f03f54c6ed4944d2a201a7b6a
Danny <agyapongdaniel7777@gmail.com>
2021-01-14 17:07:29 +0000

    browser router bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-import { Route, Router, Switch } from "react-router-dom";
+import { Route, BrowserRouter, Switch } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home/Home";
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <div className="app">
         <Switch>
+          <Route path="/login">
+            <h1>This is login page</h1>
+          </Route>
           <Route path="/">
-            <Home/>
+            <Home />
           </Route>
         </Switch>
       </div>
-    </Router>
+    </BrowserRouter>
   );
 }
 
