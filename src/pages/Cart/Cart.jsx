3143d693404b648a09a429752b8463c07b1fbd7c
Danny <agyapongdaniel7777@gmail.com>
2021-01-19 00:45:37 +0000

    cart structured based on items in the basket

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -2,47 +2,61 @@ import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import "./Cart.css";
 import CartProduct from "../../components/CartProduct/CartProduct";
-
+import { useStateValue } from "../../providers/StateProvider";
 const Cart = () => {
+  //Here we want to access the current basket length
+  //And make changes the ui (0 for empty basket otherwise the items in the basket is shown)
+  const [{ basket }] = useStateValue();
+
   return (
     <div>
       <Navbar />
-      {/* CartDivider */}
-      <div className="cart-divider">
-        {/* CartLeftSection */}
-        <div className="cart-left-section">
-          {/* Image contains the advert */}
-          <img
-            src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
-            alt="BannerImage"
-            className="cart-banner-img"
-          />
-          <h2 className="header-text">Your Shopping Basket</h2>
-
-          {/* The list of checkout  products will be shown here */}
-          <CartProduct
-            id={1}
-            title=" Lorem ipsum dolor sit amet consectetur adipisicing elit. At, iusto
-          culpa alias aliquid quo similique veritatis enim inventore explicabo
-          dolores."
-            price={133.35}
-            ratings={4}
-            imgUrl="https://m.media-amazon.com/images/I/6186EOdL8eL._AC_UL640_FMwebp_QL65_.jpg"
-          />
+      {basket.length === 0 ? (
+        <div>
+          <h2 className="header-text">Your Shopping Basket is Empty </h2>
+          <p>You have no shopping items in your list</p>
         </div>
-        <div className="cart-right-section">
-          <div className="sub-total-section">
-            <h1>
-              Subtotal (1 items) : <strong>$11.96</strong>
-            </h1>
-            <div className="checkbox-gift-section">
-              <input type="checkbox" name="Gift" id="" />
-              <small>This order contains a gift</small>
+      ) : (
+        <div>
+          {/* CartDivider */}
+          <div className="cart-divider">
+            {/* CartLeftSection */}
+            <div className="cart-left-section">
+              {/* Image contains the advert */}
+              <img
+                src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+                alt="BannerImage"
+                className="cart-banner-img"
+              />
+              <h2 className="header-text">Your Shopping Basket</h2>
+
+              {/* The list of checkout  products will be shown here */}
+              
+              <CartProduct
+                id={1}
+                title=" Lorem ipsum dolor sit amet consectetur adipisicing elit. At, iusto
+            culpa alias aliquid quo similique veritatis enim inventore explicabo
+            dolores."
+                price={133.35}
+                ratings={4}
+                imgUrl="https://m.media-amazon.com/images/I/6186EOdL8eL._AC_UL640_FMwebp_QL65_.jpg"
+              />
             </div>
-            <button className="checkout-btn">Proceed to Checkout</button>
+            <div className="cart-right-section">
+              <div className="sub-total-section">
+                <h1>
+                  Subtotal (1 items) : <strong>$11.96</strong>
+                </h1>
+                <div className="checkbox-gift-section">
+                  <input type="checkbox" name="Gift" id="" />
+                  <small>This order contains a gift</small>
+                </div>
+                <button className="checkout-btn">Proceed to Checkout</button>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
