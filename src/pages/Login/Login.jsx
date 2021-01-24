999308e5e580b54ece059885b4c01a556ee11e7a
Danny <agyapongdaniel7777@gmail.com>
2021-01-24 20:23:29 +0000

    login structure complete

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -8,6 +8,24 @@ const Login = () => {
           src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
           alt="Amazon logo"
         />
+        <div className="login-box">
+          <h1>Sign In</h1>
+          <div className="email-box">
+            <span>E-mail</span>
+            <input type="email" id="emailId" />
+          </div>
+          <div className="password-box">
+            <span>Password</span>
+            <input type="password" id="password" />
+          </div>
+          <button type="submit">Sign In</button>
+          <small>
+            By signing-in you agree to Amazon's Condition of Use & Sale. Please
+            see our Privacy Notice, Our Cookies Notice and our interest-Based
+            Ads Notice
+          </small>
+          <button type="submit">Create your Amazon Account</button>
+        </div>
       </div>
     </div>
   );
